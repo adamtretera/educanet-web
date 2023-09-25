ed952ed1dbfb37b3d4ee79fd7b51c949ad3e2dd1
Adam Tretera <adam.tretera@ackee.cz>
2023-09-25 21:10:06 +0200

    :sparkles: Fix landing page

--- landing/src/App.vue
+++ landing/src/App.vue
@@ -3,25 +3,6 @@ import topics from '../../topics/topics.js'
 </script>
 
 <template>
-  <section class="bg-gray-700">
-    <div class="max-w-screen-xl mx-auto px-2 sm:px-6 lg:px-8">
-      <h1 class="text-3xl pt-12">
-        Presentations by Adam Tretera
-      </h1>
-      <h2 class="text-s pb-12">
-      </h2>
-
-      <a
-        class="github-fork-ribbon"
-        href="https://github.com/tboerger/talks"
-        data-ribbon="Fork me on GitHub"
-        title="Fork me on GitHub"
-      >
-        Fork me on GitHub
-      </a>
-    </div>
-  </section>
-
   <div class="max-w-screen-xl mx-auto container">
     <div class="flex flex-wrap">
       <div class="sm:w-1/3 w-1/2 flex flex-col" v-for="topic in topics" v-bind:key="topic.title">
